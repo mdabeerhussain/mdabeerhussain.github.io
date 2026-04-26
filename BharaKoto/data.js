2ec8122982581f4f67712f26965c63e2042e7998
Md Abeer Hussain <abeer.rumc@gmail.com>
2026-04-27 01:15:28 +0600

    Add files via upload

---
+++ BharaKoto/data.js
@@ -0,0 +1,42 @@
+const busData = {
+  routes: [
+    {
+      id: "এ-৩১৭",
+      name: "দিয়াবাড়ী হতে পোস্তগোলা",
+      farePerKm: 2.53,
+      minFare: 10,
+      brtaLink: "http://www.brta.gov.bd/", // <-- Add your specific PDF/Image link here
+      stops: [
+        { name: "দিয়াবাড়ী চৌরাস্তা", dist: 0.0 },
+        { name: "মাস্কট প্লাজা", dist: 2.5 },
+        { name: "এয়ারপোর্ট", dist: 4.5 },
+        { name: "কুড়িল ফ্লাইওভার", dist: 8.5 },
+        { name: "নতুন বাজার", dist: 12.1 },
+        { name: "বাড্ডা", dist: 14.6 },
+        { name: "রামপুরা", dist: 16.7 },
+        { name: "খিলগাঁও ফ্লাইওভার", dist: 20.7 },
+        { name: "টিটিপাড়া", dist: 22.4 },
+        { name: "সায়েদাবাদ", dist: 23.4 },
+        { name: "যাত্রাবাড়ী", dist: 24.8 },
+        { name: "পোস্তগোলা", dist: 27.3 },
+      ],
+    },
+    {
+      id: "এ-৩০৭",
+      name: "মিরপুর-১৪ হতে ইপিজেড",
+      farePerKm: 2.53,
+      minFare: 10,
+      brtaLink: "http://www.brta.gov.bd/", // <-- Add your specific PDF/Image link here
+      stops: [
+        { name: "মিরপুর-১৪", dist: 0.0 },
+        { name: "মিরপুর-১০", dist: 2.1 },
+        { name: "মিরপুর-২", dist: 3.6 },
+        { name: "মিরপুর-১", dist: 4.6 },
+        { name: "গাবতলী", dist: 6.6 },
+        { name: "সাভার", dist: 20.1 },
+        { name: "নবীনগর", dist: 28.1 },
+        { name: "ইপিজেড", dist: 32.7 },
+      ],
+    },
+  ],
+};
